f22b0df31a93f5db147e32bc88ac00e82648271c
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-09 08:22:17 -0700

    Added SQL for sign_up
    Added the SQL to the /sign_up route to insert new user info into db.

--- flask_app/app.py
+++ flask_app/app.py
@@ -82,9 +82,63 @@ def popular_polls(name=None):
     return render_template("popular_polls.html", name=name)
 
 # add GET/POST with login info? and figure out how to do sign up / account creation
-# @app.route('/signup')
-# def sign_up():
-#     return "This is where you will create a new account!"
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    '''
+    This function adds a new user to the database
+    '''
+    if request.method == 'POST' and 'User Name' in request.form and 'Password' in request.form and 'Email' in request.form and 'First' in
+    reuest.form and 'Last' in request.form:
+        # Store form inputs as variables
+        user_name = request.form['User Name']
+        password = request.form['Password']
+        email = request.form['Email']
+        first = request.form['First']
+        last = request.form['Last']
+        
+        # user_id must be unique!
+        # Create random 9-digit id for user_id. Query database to see if id exists already.
+        new_id = randint(100000000, 999999999)
+        result = c.execute("SELECT * FROM Users WHERE user_id='{}'".format(new_id)).fetchone()
+        
+        # c.execute will return nothing if the id does not exist.
+        # If user_id already exists, randomly select new 9-digit id until one is chose that does not exist already.
+        if result != None:
+            while result != None:
+                new_id = randint(100000000, 999999999)
+                result = c.execute("SELECT * FROM Users WHERE user_id='{}'".format(new_id)).fetchone()
+
+        # user_name must be unique. return error message indicating user name is taken if not unique
+        new_name = c.execute("SELECT * FROM Users WHERE user_name='{}'".format(user_name)).fetchone()
+        if new_name != None:
+            return render_template('sign_up.html', message="User name taken. Please try again.")
+        # Chech regex match to verify valid email address.
+        if not re.match(r'([A-Za-z\.!#$%\*()0-9]+@[A-Za-Z0-9]+\.[a-zA-Z]{2,3}', email):
+            return render_template('sign_up.html', message="Please enter a valid email.")
+        # email must be unique. return error message if email is associated with an account
+        new_email = c.execute("SELECT * FROM Users WHERE user_name='{}'".format(email)).fetchone()
+        if new_email = None:
+            return render_template('sign_up.html', message="Email address already associated with an account. Please try again.")
+        
+        timestamp = datetime.datetime.now()
+
+        # Connect to database
+        conn = sqlite3.connect("db", timeout=10)
+        c = conn.cursor()
+        
+        #insert values into table
+        sql = "INSERT INTO Users VALUES (?, ?, ?, ?, ?, ?, ?)"
+        new_user = (new_id, user_name, first, last, email, password, timestamp)
+        c.execute(sql, new_user)
+        
+        # Commit changes and close database
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for(user_page, user_name=user_name))
+    else:
+        return render_template('sign_up.html', message='please complete the form')
+    return render_template('sign_up.html')
 
 # @app.route('/user')
 # def user_dashboard():
