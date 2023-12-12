657d3bebad4271036f7c38fabc50a1c511216845
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-11 20:26:25 -0700

    Update app.py
    bug fixes

--- flask_app/app.py
+++ flask_app/app.py
@@ -127,7 +127,7 @@ def log_in(name=None):
     else:
         return render_template("login_page.html")
 
-@app.route('logout')
+@app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for("login"))
