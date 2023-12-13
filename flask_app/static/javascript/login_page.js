a0e415aa95781400e7251705d8cae5be90f84014
Catalina Escalona <catalinaescalona@gmail.com>
2023-12-13 13:28:49 +0100

    updated frontend for login page

---
+++ flask_app/static/javascript/login_page.js
@@ -0,0 +1,43 @@
+// JavaScript page for Pollaris Login Page
+// Authors: Escalona, Sansbury, Webster
+// Written December 2023
+// CSPB 3308 - Fall 2023 - Knox - Software Development Tools and Methods
+// University of Colorado Boulder
+
+$(document).ready(function() {
+    // clicking logo redirects to homepage
+    $("#logo").on("click", function() {
+        $.ajax({
+            type: 'GET',
+            url: '/redirect_home',
+            success: function(response) {
+                // redirect to home URL
+                window.location.href = response.home_url;
+            }
+        });
+    });
+    
+    // clicking login button redirects to login page
+    $("#login-btn").on("click", function() {
+        $.ajax({
+            type: 'GET',
+            url: '/redirect_login',
+            success: function(response) {
+                // redirect to login URL
+                window.location.href = response.login_url;
+            }
+        });
+    });
+
+    // clicking sign up button redirects to sign up page
+    $("#sign-up-btn").on("click", function() {
+        $.ajax({
+            type: 'GET',
+            url: '/redirect_signup',
+            success: function(response) {
+                // redirect to signup URL
+                window.location.href = response.signup_url;
+            }
+        });
+    });
+});
