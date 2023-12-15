a53de4dde15ddc44591d1ab6fabf20e587918e76
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-14 21:43:43 -0700

    Update app.py
    chagnes to test route for create

--- flask_app/app.py
+++ flask_app/app.py
@@ -221,12 +221,13 @@ def redirect_create():
 # GET AJAX METHOD TO WORK HERE
 @app.route('/create_test', methods=["GET", "POST"])
 def create_poll_test(name=None):
-    poll_data = request.get_json(force=True)  # This is a python dictionary
-    if poll_data:
-        st = ""
-        for key,value in poll_data:
-            st =+ (str(key)+" "+str(value))
-        return st
+    if request.method == 'POST':
+        poll_data = request.get_json(force=True)  # This is a python dictionary
+        if poll_data:
+            st = ""
+            for key,value in poll_data:
+                st =+ (str(key)+" "+str(value))
+            return st
 
     return render_template(create_poll.html)
     
