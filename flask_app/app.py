abbd5dda43646e5074e5af1d954550e589dc2989
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-15 09:14:45 -0700

    Update app.py
    testing ajax

--- flask_app/app.py
+++ flask_app/app.py
@@ -222,7 +222,9 @@ def redirect_create():
 @app.route('/create_test', methods=["GET", "POST"])
 def create_poll_test(name=None):
     if request.method == 'POST':
-        poll_data = request.json
+        poll_data = {}
+        poll_data['questions'] = request.json['questions']
+        poll_data['answers'] = request.json['answers']
         if poll_data:
             poll_data = jsonify(poll_data)
             print("POLL DATA: ", poll_data)
