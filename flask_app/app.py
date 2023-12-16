0ae50f42406d58ae6449972ca12b39c62c4537d1
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-16 12:35:55 -0700

    Update app.py
    commenting out redirect to test how poll id is being stored as a variable

--- flask_app/app.py
+++ flask_app/app.py
@@ -131,7 +131,8 @@ def index(name=None):
     
     if request.method == "POST":
         poll = request.form['pollid']
-        return redirect(url_for("vote_test", poll_id=poll))
+        return "<p>"+poll+"</p>"
+        #return redirect(url_for("vote_test", poll_id=poll))
     
     '''Renders an HTML template with the Pollaris Homepage'''
     return render_template("homepage.html", name=name)
