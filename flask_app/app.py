65603c091f4cba5151bf572602334d5ed602a170
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-16 12:39:19 -0700

    Update app.py
    casting poll_id input being passed from homepage to int to match db format

--- flask_app/app.py
+++ flask_app/app.py
@@ -131,8 +131,7 @@ def index(name=None):
     
     if request.method == "POST":
         poll = request.form['pollid']
-        return "<p>"+poll+"</p>"
-        #return redirect(url_for("vote_test", poll_id=poll))
+        return redirect(url_for("vote_test", poll_id=int(poll)))
     
     '''Renders an HTML template with the Pollaris Homepage'''
     return render_template("homepage.html", name=name)
