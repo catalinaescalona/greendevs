8ff7092014b757449e7ba3ce1be5ef353277173e
gregweb4039 <120431515+gregweb4039@users.noreply.github.com>
2023-12-16 12:46:09 -0700

    Update app.py
    syntax error

--- flask_app/app.py
+++ flask_app/app.py
@@ -131,7 +131,7 @@ def index(name=None):
     
     if request.method == "POST":
         poll = request.form['pollid']
-        return redirect(url_for("vote_test", int(poll))
+        return redirect(url_for("vote_test", int(poll)))
     
     '''Renders an HTML template with the Pollaris Homepage'''
     return render_template("homepage.html", name=name)
